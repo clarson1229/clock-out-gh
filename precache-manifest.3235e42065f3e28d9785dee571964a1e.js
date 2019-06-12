2b6a55f498078f205633a4be1d497e0cc9453bab
System Administrator <root@c02yl398jgh5.northamerica.cerner.net>
2019-06-12 09:44:26 -0500

    Updates

---
+++ precache-manifest.3235e42065f3e28d9785dee571964a1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c8a33767f5c4130d3ddf12d03192ad",
+    "url": "/clock-out-gh/index.html"
+  },
+  {
+    "revision": "71f413992ed96537b8aa",
+    "url": "/clock-out-gh/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clock-out-gh/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "71f413992ed96537b8aa",
+    "url": "/clock-out-gh/static/js/main.ce9d8071.chunk.js"
+  },
+  {
+    "revision": "df4bb08772ff301ffb73",
+    "url": "/clock-out-gh/static/js/runtime~main.3c152f60.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clock-out-gh/static/media/logo.5d5d9eef.svg"
+  }
+]);
