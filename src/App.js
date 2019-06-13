74ce123541b1cc01b471bc18d4098facc07f146c
Larson <cl058108@c02yl398jgh5.northamerica.cerner.net>
2019-06-13 09:16:58 -0500

    Background added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Form from "./Components/Form";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App gradient">
       <div className='HeaderStuff'>
         <h1>When should I clock out?</h1>
         <h2>Input: The hours worked and The time you clocked in.</h2>
